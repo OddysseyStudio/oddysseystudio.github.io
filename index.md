31192f8e2a30c5ecaee3f7423c68eb6941c57011
Gio- <saffirio.giorgio@gmail.com>
2017-11-02 02:10:03 +0100

    New Site

---
+++ index.md
@@ -0,0 +1,26 @@
+---
+layout: home/home-index
+---
+
+{% capture story %}
+Born in the mid-summer of 2017, Oddyssey Studio set sails in the amazing sea of the Game Development with its first project: The Eye.
+<br>We are student and ex-student from the Game Development School "Event Horizon" with a past in other narrative media. That's why our game have a deeper layer of entertainment, there for everyone who want to look and find an amazing story other than a unique game.
+{% endcapture %}
+
+{% include home/home-story.html %}
+
+{% capture team-description %}
+Lorem Ipsum Dolor Sit Amet 
+{% endcapture %}
+
+{% include home/home-team.html %}
+
+{% capture wip-title %}
+The Eye
+{% endcapture %}
+
+{% capture wip-description %}
+<b>The Eye</b> is text adventure game played like a card game. The story is setted in a unique lovecraftian setting that takes inspiration from the work of the Mythos still unexplored by other videogame media. The game features over <b>728 narrative</b>            branches. divided in<b> 70+ event cards</b>, each with an astonishing illustration. The Eye will be launched soon for <b>Android </b>and <b>iOS </b>devices.
+{% endcapture %}
+
+{% include home/home-wip.html %}
---
+++ js/main.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+  var scrollTop = 0;
+  var navbar = $('.navbar');
+  var brand = $('.navbar .navbar-brand');
+  $(window).scroll(function(){
+    scrollTop = $(window).scrollTop();
+
+    if (scrollTop >= 100 && navbar.is('.navbar-light')) {
+      navbar.removeClass('navbar-transparent');
+	  navbar.removeClass('navbar-light');
+	  brand.addClass('navbar-brand-black');
+	  
+
+    } else if (scrollTop < 100 && !navbar.is('.navbar-light')) {
+		navbar.addClass('navbar-light');
+
+		if(!$(".navbar-collapse").hasClass("show"))
+		{
+			navbar.addClass('navbar-transparent');
+			brand.removeClass('navbar-brand-black');
+		}
+    } 
+    
+  }); 
+
+  $("button.navbar-toggler").on('click', function(event) {
+      // Prevent default anchor click behavior
+		event.preventDefault();
+
+		if(!$(".navbar-collapse").hasClass("show"))
+		{
+			navbar.removeClass('navbar-transparent');
+			brand.addClass('navbar-brand-black');
+		}else{
+			if(navbar.hasClass('navbar-light')){
+				navbar.addClass('navbar-transparent');
+				brand.removeClass('navbar-brand-black');
+			}
+		}
+  });
+  
+  $("a.js-scroll-trigger").on('click', function(event) {
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+
+  } 
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('#story .container', {
+    duration: 1000,
+    delay: 200
+  }, 200);
+  sr.reveal('#team .container', {
+    duration: 1000,
+    delay: 200
+  });
+  sr.reveal('#wip .container', {
+    duration: 1000,
+    scale: 0.3,
+    distance: '0px'
+  }, 200);
+
+  
+});
